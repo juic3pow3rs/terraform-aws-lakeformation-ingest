5e9c98a40ac02a4203400e0220e841c84c1b68d9
juic3pow3rs <juic3pow3rs@users.noreply.github.com>
2022-11-21 12:27:29 +0100

    update modules

--- main.tf
+++ main.tf
@@ -21,20 +21,19 @@ module "s3_bucket" {
 
   environment      = local.environment
   role             = local.role
-  project          = local.project
   template_version = local.template_version
   team             = local.team
 }
 
 module "lakeformation" {
-  source = "./terraform-aws-modules/lakeformation"
+  source = "./terraform-aws-modules/lake_formation"
   providers = {
     aws = aws.ffm
   }
 
-  admin_user_arn = data.aws_iam_session_context.current.issuer_arn
-  s3_bucket_arn = module.s3_bucket.s3_bucket_arn
-  s3_bucket_name = module.s3_bucket.s3_bucket_name
+  admin_user_arn = data.aws_caller_identity.current.arn
+  s3_bucket_arn = module.s3_bucket.bucket_arn
+  s3_bucket_name = module.s3_bucket.bucket_name
   file_name = "demoLakeData.csv"
   file_source = "./demoLakeData.csv"
 
@@ -45,3 +44,19 @@ module "lakeformation" {
   team             = local.team
 }
 
+module "glue_catalog" {
+  source = "./terraform-aws-modules/glue_catalog"
+  providers = {
+    aws = aws.ffm
+  }
+
+  s3_bucket_name = module.s3_bucket.bucket_name
+  file_name = "demoLakeData.csv"
+  folder_path = "bronze/ingestion"
+
+  environment      = local.environment
+  role             = local.role
+  project          = local.project
+  template_version = local.template_version
+  team             = local.team
+}
